2acb5999ec122a5a882eb7f2b4a216e0a470e901
王永政 <yongzheng13@gmail.com>
2018-12-08 11:51:40 +0800

    Navigation侧滑效果

---
+++ app/src/main/java/com/love/jax/activity/NavigationActivity.java
@@ -0,0 +1,37 @@
+package com.love.jax.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.love.jax.R;
+
+/**
+ * TODO google规范的侧滑效果
+ * @see android.support.design.widget.NavigationView
+ * DrawerLayout+ NavigationView结合使用
+ * 注意：使用NavigationView，需要依赖项目：Design项目。
+ * 同时还需要依赖recyclerView项目和CardView项目
+ */
+public class NavigationActivity extends BaseActivity {
+
+
+    @Override
+    protected void initJestListener() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_navigation;
+    }
+}
