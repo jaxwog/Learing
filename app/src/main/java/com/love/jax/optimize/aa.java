f4a0871d1ba39208468857d75993355bb9e74956
王永政 <yongzheng13@gmail.com>
2019-08-25 17:21:31 +0800

    热修复

---
+++ app/src/main/java/com/love/jax/optimize/aa.java
@@ -0,0 +1,9 @@
+package com.love.jax.optimize;
+
+/**
+ * com.love.jax.optimize
+ * Created by jax on 2019-08-23 15:00
+ * TODO:
+ */
+public class aa {
+}
---
+++ app/src/main/java/com/love/jax/optimize/fixbug/MyTestClass.java
@@ -0,0 +1,13 @@
+package com.love.jax.optimize.fixbug;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MyTestClass {
+    public void testFix(Context context) {
+        int i = 10;
+        int a = 1;
+        Toast.makeText(context, "shit:" + i / a, Toast.LENGTH_SHORT).show();
+    }
+}
